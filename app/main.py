da05d259f7177a36e3dc5027733afe55a0d5e735
Alisson Cruz <alissonpdc@gmail.com>
2024-06-20 21:19:27 -0300

    chore: basic api functions

--- app/main.py
+++ app/main.py
@@ -1,32 +1,43 @@
-from fastapi import FastAPI, HTTPException, status, Query, Response
+import utils
+
+from fastapi import FastAPI, HTTPException, status
 from pokemon import Pokemon
-from utils import find_pokemon_index
+
 
 app = FastAPI()
 
 
 pokemon_list = [
-        { "name": "pikachu", "type": "eletric" },
-        { "name": "charmander", "type": "fire" }
-    ]
+    { "id": 1, "name": "Bulbasaur", "type": "grass", "evolution_level": 0, "next_evolution_id": 2, "previous_evolution_id": None },
+    { "id": 2, "name": "Ivysaur", "type": "grass", "evolution_level": 1, "next_evolution_id": 3, "previous_evolution_id": 1 },
+    { "id": 3, "name": "Venusaur", "type": "grass", "evolution_level": 2, "next_evolution_id": None, "previous_evolution_id": 2 },
+    { "id": 4, "name": "Charmander", "type": "fire", "evolution_level": 0, "next_evolution_id": 5, "previous_evolution_id": None }
+]
 
 
 @app.get("/pokemons")
 def get_all_pokemons():
-    return { "status": "OK", "pokemons": pokemon_list }
+    return  { 
+                "count": len(pokemon_list),
+                "results": pokemon_list
+            }
 
 
 @app.post("/pokemons", status_code=status.HTTP_201_CREATED)
 def create_pokemon(pokemon: Pokemon):
-    pokemon_list.append(pokemon.dict())
-    return { "status": "OK", "pokemons": pokemon_list }
+    pokemon_list.append(pokemon.model_dump())
+    return {}
 
-@app.get("/pokemons/{name}")
-def get_pokemon_by_name(name: str):
-    idx = find_pokemon_index(pokemon_list, name)
-    if idx is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Pokemon {name} doesnt exist."
-        )
-    return pokemon_list[idx]
+@app.get("/pokemons/{search}", status_code=status.HTTP_200_OK)
+def get_pokemon(search: str):
+    _,result = utils.find_pokemon(pokemon_list, search)
+    if result is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return result
+
+@app.delete("/pokemons/{search}", status_code=status.HTTP_202_ACCEPTED)
+def delete_pokemon(search: str):
+    result = utils.delete_pokemon(pokemon_list, search)
+    if result is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return {}
--- app/pokemon.py
+++ app/pokemon.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
 
 class Pokemon(BaseModel):
+    id: int
     name: str
     type: str
+    evolution_level: int
+    next_evolution_id: int
+    previous_evolution_id: int
